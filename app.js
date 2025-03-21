21ba9bfe700ff339b1c07297ca7d908fe3148e2d
Ashwin Kapile <ashwinkapile2002@gmail.com>
2025-03-21 16:31:28 -0700

    Added a comment

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const reviewRouter = require('./routes/reviewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
 const viewRouter = require('./routes/viewRoutes');
 
+//Starts express app
 const app = express();
 
 // Use CORS with default settings (allows all origins)
